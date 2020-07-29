e9667edbae3a28e2af837e62fbc6fad90b08e4ec
syedtakbar <sakbar01@icloud.com>
2020-07-29 10:21:13 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,9 @@
 import { BounceBall } from './Ball';
-// Try uncommenting the line below :)
-//import pad from 'left-pad'; alert(pad);
- 
-// Set gravity effect on the ball.
-// 1 = Earth, .16 = Moon, 2 = Jupiter.
-const GRAVITY = 1;
 
-// Color of the ball.
+
+const GRAVITY = .16;
 const COLOR = 'red';
 
-// Bounce the ball!
 BounceBall({
   color: COLOR,
   gravity: GRAVITY
